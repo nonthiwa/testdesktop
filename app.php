f1e553b9dd8f0f9dd1e3e7cec5ce64eb2b5ee36f
nonthiwa <nonthiwaht123@gmail.com>
2024-06-03 22:51:10 +0700

    Update app.php

--- app.php
+++ app.php
@@ -1,1 +1,13 @@
-hello world
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Test index</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+<?php echo 'Hello world'; ?>
+</body>
+</html>
